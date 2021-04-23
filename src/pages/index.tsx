939e8fb065cf1b1351fb8a62401f541ab8186fb3
Weverton Icaro <weverton-Icaro@users.noreply.github.com>
2021-04-22 23:21:11 -0300

    Fourth day, increasing play and pause functions in the Player.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { useContext } from 'react';
 import { GetStaticProps } from 'next';
 import Image from 'next/image';
 import Link from 'next/link';
@@ -5,7 +6,7 @@ import { format, parseISO } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
 import { api } from '../services/api';
 import { convertDurationToTimeString } from '../utils/convertDurationToTimeString';
-
+import { PlayerContext } from '../contexts/PlayerContext';
 import styles from './home.module.scss';
 
 type Episode = {
@@ -26,6 +27,8 @@ type HomeProps = {
 };
 
 export default function Home({ lastedEpisodes, allEpisodes }: HomeProps) {
+	const { play } = useContext(PlayerContext);
+
 	return (
 		<div className={styles.homepage}>
 			<section className={styles.latestEpisodes}>
@@ -52,7 +55,7 @@ export default function Home({ lastedEpisodes, allEpisodes }: HomeProps) {
 									<span>{episode.durationAsString}</span>
 								</div>
 
-								<button type="button">
+								<button type="button" onClick={() => play(episode)}>
 									<img src="/play-green.svg" alt="Listen Now" />
 								</button>
 							</li>
